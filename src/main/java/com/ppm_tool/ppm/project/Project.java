64f0dbefe55580449025ce418f2d3876b7bfe57f
Josh Burrill <joshua.burrill.1@gmail.com>
2021-03-14 15:44:05 -0400

    Added Project object and interface

---
+++ src/main/java/com/ppm_tool/ppm/project/Project.java
@@ -0,0 +1,108 @@
+package com.ppm_tool.ppm.project;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+@Entity
+public class Project {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String projectName;
+	private String projectIdentified;
+	private String description;
+	private Date start_date;
+	private Date end_date;
+	
+	private Date created_At;
+	private Date update_At;
+	
+	public Project() {}
+
+	@PrePersist
+	protected void onCreate() {
+		this.created_At = new Date();
+	}
+
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.update_At = new Date();
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getProjectIdentified() {
+		return projectIdentified;
+	}
+
+	public void setProjectIdentified(String projectIdentified) {
+		this.projectIdentified = projectIdentified;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+
+	public Date getEnd_date() {
+		return end_date;
+	}
+
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+
+	public Date getCreated_At() {
+		return created_At;
+	}
+
+	public void setCreated_At(Date created_At) {
+		this.created_At = created_At;
+	}
+
+	public Date getUpdate_At() {
+		return update_At;
+	}
+
+	public void setUpdate_At(Date update_At) {
+		this.update_At = update_At;
+	}
+
+
+	
+	
+}
---
+++ src/main/java/com/ppm_tool/ppm/repository/ProjectRepository.java
@@ -0,0 +1,5 @@
+package com.ppm_tool.ppm.repository;
+
+public interface ProjectRepository {
+
+}
