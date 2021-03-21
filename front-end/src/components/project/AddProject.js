b624628303175fa4c59bf07f4267f2ebfb2c272c
Josh Burrill <joshua.burrill.1@gmail.com>
2021-03-20 21:33:46 -0400

    Created AddProject component with form

--- front-end/src/components/project/AddProject.js
+++ front-end/src/components/project/AddProject.js
@@ -1,10 +1,79 @@
-import React from 'react'
+import { FormLabel } from '@chakra-ui/form-control';
+import { Input } from '@chakra-ui/input';
+import { Container, Text } from '@chakra-ui/layout';
+import { Textarea } from '@chakra-ui/textarea';
+import React, { useState } from 'react'
+import { Button } from '@chakra-ui/react'
 
 const AddProject = () => {
+    const [projectFields, setFields] = useState({
+        projectName: "",
+        projectIdentified: "",
+        description: "",
+        start_date: new Date(),
+        end_date: ""
+    });
+
+    const handleChange = (event) => {
+        setFields(fields => ({ ...fields, [event.target.name]:event.target.value  }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault
+        console.log({projectFields})
+    }
+
     return (
-        <div>
-            Hello
-        </div>
+        <Container>
+            <Text marginTop="15px" marginBottom="25px" marginLeft="150px" fontSize="30px">Creat/Edit Project</Text>
+            <form onSubmit={handleSubmit}>
+                <Input
+                    name="projectName"
+                    value={projectFields.name}
+                    onChange={handleChange}
+                    placeholder="Project name"
+                    size="md"
+                    marginBottom="15px"
+                />
+                <Input
+                    name="projectIdentified"
+                    value={projectFields.id}
+                    onChange={handleChange}
+                    placeholder="Project ID"
+                    size="md"
+                    marginBottom="15px"
+                />
+                <Textarea
+                    name="description"
+                    value={projectFields.description}
+                    onChange={handleChange}
+                    placeholder="Project description"
+                    size="md"
+                    marginBottom="25px"
+                />
+                <FormLabel>Start date</FormLabel>
+                <Input
+                    type="date"
+                    name="start_date"
+                    value={projectFields.name}
+                    onChange={handleChange}
+                    size="md"
+                    marginBottom="15px"
+                />
+                <FormLabel>Est. end date</FormLabel>            
+                <Input
+                    type="date"
+                    name="end_date"
+                    value={projectFields.id}
+                    onChange={handleChange}
+                    size="md"
+                    marginBottom="25px"
+                />
+                <div style={{ float: "right" }}>
+                    <Button type="submit" colorScheme="blue" isLoading={false}>Submit</Button>
+                </div>
+            </form>
+        </Container>
     )
 }
 
